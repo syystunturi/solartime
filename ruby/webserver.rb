beefca2fbc43caf554b3f1bf7e2f9787c0465142
Jari Lammi <jari.lammi@valuecode.com>
2014-05-10 10:18:07 +0300

    Enhances the coordinates output on the web server.

--- ruby/webserver.rb
+++ ruby/webserver.rb
@@ -33,7 +33,7 @@ p { color: #3399ff }"
   end
 
   post '/' do
-    # matches "GET /?city=Helsinki&location=KP20LE"
+    # matches "POST /?city=Helsinki&location=KP20LE"
     @city = params[:city]
     @location = params[:location]
 
@@ -45,20 +45,28 @@ Locator: <input type="text" name="location" value="<%= @location %>"><br>
 
     if @city != nil
       longitude, latitude = solartime.web_server_solartime @city, @location
+
       if longitude > 0
         west_or_east = 'E'
       else
         west_or_east = 'W'
       end
+
       if latitude > 0
         south_or_north = 'N'
       else
         south_or_north = 'S'
       end
+
+      longitude_full = longitude.to_i.abs
+      longitude_minutes = 60 * (longitude.abs - longitude_full)
+      latitude_full = latitude.to_i.abs
+      latitude_minutes = 60 * (latitude.abs - latitude_full)
+
       sunrise_sunset_result += "<h2><%= @city %> (<%= @location %>)</h2>
 <ul>
-  <li>Longitude: #{west_or_east}#{longitude}</li>
-  <li>Latitude: #{south_or_north}#{latitude}</li>
+  <li>Longitude: #{west_or_east} #{longitude_full}&deg; #{"%.3f" % longitude_minutes}</li>
+  <li>Latitude: #{south_or_north} #{latitude_full}&deg; #{"%.3f" % latitude_minutes}</li>
 </ul>"
     end
 
