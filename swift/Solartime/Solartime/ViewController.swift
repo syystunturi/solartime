ccd2ce017c68f9b2b6b244a2764cb76ef357819b
Jari Lammi <jari.lammi@valuecode.com>
2014-09-28 16:26:10 +0300

    Adds a Swift test project for Xcode.

---
+++ swift/Solartime/Solartime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Solartime
+//
+//  Created by Jari Lammi on 28.9.2014.
+//  Copyright (c) 2014 Jari Lammi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
