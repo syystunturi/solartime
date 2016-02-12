5720db82f0f65a5ce3cb4c3c37e0b5bbbb0c1418
Jari Lammi <jari.lammi@alumni.helsinki.fi>
2016-02-12 18:22:54 +0200

    Refreshes the rspec tests.

--- ruby/spec/maidenhead_spec.rb
+++ ruby/spec/maidenhead_spec.rb
@@ -7,8 +7,8 @@ describe Maidenhead do
   describe ".locator_to_coordinates" do
     it "calculates the coordinates of Helsinki correctly" do
       longitude, latitude = locator_to_coordinates( "KP20LE" )
-      longitude.should eq(24.958333333333336)
-      latitude.should eq(60.1875)
+      expect(longitude).to eq(24.958333333333336)
+      expect(latitude).to eq(60.1875)
     end
   end
 
--- ruby/spec/solartime_spec.rb
+++ ruby/spec/solartime_spec.rb
@@ -10,18 +10,15 @@ describe Solartime do
 
   describe ".command_line_solartime" do
     it "calculates the coordinates of Berlin correctly" do
-      output = capture :stdout do
-        @solartime.command_line_solartime "Berlin", "JO62QM"
-      end
-      expect(output).to include 'Berlin (JO62QM: 13.375 52.521)'
+      expect { @solartime.command_line_solartime "Berlin", "JO62QM" }.to output("Berlin (JO62QM: 13.375 52.521)\n").to_stdout
     end
   end
 
   describe ".web_server_solartime" do
     it "calculates the coordinates of Munich correctly" do
       a, b = @solartime.web_server_solartime "München", "JN58SD"
-      a.should eq(11.541666666666666)
-      b.should eq(48.145833333333336)
+      expect(a).to eq(11.541666666666666)
+      expect(b).to eq(48.145833333333336)
     end
   end
 
